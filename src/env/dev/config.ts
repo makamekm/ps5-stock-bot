7bdc1cc05abe64daedaf3450810637134f40c772
makame <makame@mail.ru>
2021-01-13 00:18:42 +0100

    fixes

--- src/env/dev/config.ts
+++ src/env/dev/config.ts
@@ -27,9 +27,7 @@ export const BROWSER_ARGS = [
     ? [
         "--disable-gpu",
         "--disable-dev-shm-usage",
-        "--disable-setuid-sandbox",
         "--no-first-run",
-        "--no-sandbox",
         "--no-zygote",
         "--single-process",
       ]
--- src/env/prod/config.ts
+++ src/env/prod/config.ts
@@ -28,9 +28,7 @@ export const BROWSER_ARGS = [
     ? [
         "--disable-gpu",
         "--disable-dev-shm-usage",
-        "--disable-setuid-sandbox",
         "--no-first-run",
-        "--no-sandbox",
         "--no-zygote",
         "--single-process",
       ]
